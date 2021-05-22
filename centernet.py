4d229182939793d8555afe9694103f57c7bbdffb
Bubbliiiing <47347516+bubbliiiing@users.noreply.github.com>
2021-05-22 11:29:44 +0800

    Add files via upload

--- centernet.py
+++ centernet.py
@@ -1,5 +1,6 @@
 import colorsys
 import os
+import time
 
 import numpy as np
 import torch
@@ -27,9 +28,10 @@ class CenterNet(object):
     _defaults = {
         "model_path"        : 'model_data/centernet_resnet50_voc.pth',
         "classes_path"      : 'model_data/voc_classes.txt',
+        "backbone"          : 'resnet50',
         # "model_path"        : 'model_data/centernet_hourglass_coco.h5',
         # "classes_path"      : 'model_data/coco_classes.txt',
-        "backbone"          : "resnet50",
+        # "backbone"          : 'hourglass',
         "image_size"        : [512,512,3],
         "confidence"        : 0.3,
         # backbone为resnet50时建议设置为True
@@ -217,3 +219,77 @@ class CenterNet(object):
             draw.text(text_origin, str(label,'UTF-8'), fill=(0, 0, 0), font=font)
             del draw
         return image
+
+    def get_FPS(self, image, test_interval):
+        image_shape = np.array(np.shape(image)[0:2])
+        #---------------------------------------------------------#
+        #   给图像增加灰条，实现不失真的resize
+        #---------------------------------------------------------#
+        crop_img = letterbox_image(image, [self.image_size[0], self.image_size[1]])
+        #----------------------------------------------------------------------------------#
+        #   将RGB转化成BGR，这是因为原始的centernet_hourglass权值是使用BGR通道的图片训练的
+        #----------------------------------------------------------------------------------#
+        photo = np.array(crop_img,dtype = np.float32)[:,:,::-1]
+        #-----------------------------------------------------------#
+        #   图片预处理，归一化。获得的photo的shape为[1, 512, 512, 3]
+        #-----------------------------------------------------------#
+        photo = np.reshape(np.transpose(preprocess_image(photo), (2, 0, 1)), [1, self.image_size[2], self.image_size[0], self.image_size[1]])
+        
+        with torch.no_grad():
+            images = torch.from_numpy(np.asarray(photo)).type(torch.FloatTensor)
+            if self.cuda:
+                images = images.cuda()
+            outputs = self.centernet(images)
+
+            if self.backbone=='hourglass':
+                outputs = [outputs[-1]["hm"].sigmoid(), outputs[-1]["wh"], outputs[-1]["reg"]]
+            outputs = decode_bbox(outputs[0],outputs[1],outputs[2],self.confidence,self.cuda)
+            
+            try:
+                if self.nms:
+                    outputs = np.array(nms(outputs,self.nms_threhold))
+
+                output = outputs[0]
+                if len(output)>0:
+                    batch_boxes, det_conf, det_label = output[:,:4], output[:,4], output[:,5]
+
+                    det_xmin, det_ymin, det_xmax, det_ymax = batch_boxes[:, 0], batch_boxes[:, 1], batch_boxes[:, 2], batch_boxes[:, 3]
+                    top_indices = [i for i, conf in enumerate(det_conf) if conf >= self.confidence]
+                    top_conf = det_conf[top_indices]
+                    top_label_indices = det_label[top_indices].tolist()
+                    top_xmin, top_ymin, top_xmax, top_ymax = np.expand_dims(det_xmin[top_indices],-1),np.expand_dims(det_ymin[top_indices],-1),np.expand_dims(det_xmax[top_indices],-1),np.expand_dims(det_ymax[top_indices],-1)
+        
+                    boxes = centernet_correct_boxes(top_ymin,top_xmin,top_ymax,top_xmax,np.array([self.image_size[0],self.image_size[1]]),image_shape)
+            except:
+                pass
+
+        t1 = time.time()
+        for _ in range(test_interval):
+            with torch.no_grad():
+                outputs = self.centernet(images)
+
+                if self.backbone=='hourglass':
+                    outputs = [outputs[-1]["hm"].sigmoid(), outputs[-1]["wh"], outputs[-1]["reg"]]
+                outputs = decode_bbox(outputs[0],outputs[1],outputs[2],self.confidence,self.cuda)
+                
+                try:
+                    if self.nms:
+                        outputs = np.array(nms(outputs,self.nms_threhold))
+
+                    output = outputs[0]
+                    if len(output)>0:
+                        batch_boxes, det_conf, det_label = output[:,:4], output[:,4], output[:,5]
+
+                        det_xmin, det_ymin, det_xmax, det_ymax = batch_boxes[:, 0], batch_boxes[:, 1], batch_boxes[:, 2], batch_boxes[:, 3]
+                        top_indices = [i for i, conf in enumerate(det_conf) if conf >= self.confidence]
+                        top_conf = det_conf[top_indices]
+                        top_label_indices = det_label[top_indices].tolist()
+                        top_xmin, top_ymin, top_xmax, top_ymax = np.expand_dims(det_xmin[top_indices],-1),np.expand_dims(det_ymin[top_indices],-1),np.expand_dims(det_xmax[top_indices],-1),np.expand_dims(det_ymax[top_indices],-1)
+            
+                        boxes = centernet_correct_boxes(top_ymin,top_xmin,top_ymax,top_xmax,np.array([self.image_size[0],self.image_size[1]]),image_shape)
+                except:
+                    pass
+        t2 = time.time()
+        tact_time = (t2 - t1) / test_interval
+        return tact_time
+        
