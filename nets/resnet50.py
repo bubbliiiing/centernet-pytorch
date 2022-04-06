91b63b9d0fef2e249fbddee8266c79377f0c7946
Bubbliiiing <3323290568@qq.com>
2022-04-06 17:50:38 +0800

    update a lot

--- nets/resnet50.py
+++ nets/resnet50.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import, division, print_function
 
 import math
 import torch.nn as nn
-from torchvision.models.utils import load_state_dict_from_url
+from torch.hub import load_state_dict_from_url
 
 model_urls = {
     'resnet18': 'https://s3.amazonaws.com/pytorch/models/resnet18-5c106cde.pth',
