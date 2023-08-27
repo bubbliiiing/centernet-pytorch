e3b02db682d151c3fd6e2471a6f63b92ace3e04d
bubbliiiiing <3323290568@qq.com>
2023-08-27 19:34:29 +0800

    update seed

--- train.py
+++ train.py
@@ -10,13 +10,14 @@ import torch.backends.cudnn as cudnn
 import torch.distributed as dist
 import torch.optim as optim
 from torch.utils.data import DataLoader
+from functools import partial
 
 from nets.centernet import CenterNet_HourglassNet, CenterNet_Resnet50
 from nets.centernet_training import get_lr_scheduler, set_optimizer_lr
-from utils.callbacks import LossHistory
 from utils.callbacks import EvalCallback, LossHistory
 from utils.dataloader import CenternetDataset, centernet_dataset_collate
-from utils.utils import download_weights, get_classes, show_config
+from utils.utils import (download_weights, get_classes, seed_everything,
+                         show_config, worker_init_fn)
 from utils.utils_fit import fit_one_epoch
 
 '''
@@ -41,6 +42,11 @@ if __name__ == "__main__":
     #           没有GPU可以设置成False
     #---------------------------------#
     Cuda = True
+    #----------------------------------------------#
+    #   Seed    用于固定随机种子
+    #           使得每次独立训练都可以获得一样的结果
+    #----------------------------------------------#
+    seed            = 11
     #---------------------------------------------------------------------#
     #   distributed     用于指定是否使用单机多卡分布式运行
     #                   终端指令仅支持Ubuntu。CUDA_VISIBLE_DEVICES用于在Ubuntu下指定显卡。
@@ -222,6 +228,7 @@ if __name__ == "__main__":
     train_annotation_path   = '2007_train.txt'
     val_annotation_path     = '2007_val.txt'
     
+    seed_everything(seed)
     #------------------------------------------------------#
     #   设置用到的显卡
     #------------------------------------------------------#
@@ -237,6 +244,7 @@ if __name__ == "__main__":
     else:
         device          = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         local_rank      = 0
+        rank            = 0
 
     #----------------------------------------------------#
     #   下载预训练权重
@@ -427,9 +435,11 @@ if __name__ == "__main__":
             shuffle         = True
             
         gen             = DataLoader(train_dataset, shuffle = shuffle, batch_size = batch_size, num_workers = num_workers, pin_memory=True,
-                                    drop_last=True, collate_fn=centernet_dataset_collate, sampler=train_sampler)
+                                    drop_last=True, collate_fn=centernet_dataset_collate, sampler=train_sampler, 
+                                    worker_init_fn=partial(worker_init_fn, rank=rank, seed=seed))
         gen_val         = DataLoader(val_dataset  , shuffle = shuffle, batch_size = batch_size, num_workers = num_workers, pin_memory=True, 
-                                    drop_last=True, collate_fn=centernet_dataset_collate, sampler=val_sampler)
+                                    drop_last=True, collate_fn=centernet_dataset_collate, sampler=val_sampler, 
+                                    worker_init_fn=partial(worker_init_fn, rank=rank, seed=seed))
 
         #----------------------#
         #   记录eval的map曲线
@@ -476,9 +486,11 @@ if __name__ == "__main__":
                     batch_size = batch_size // ngpus_per_node
                     
                 gen             = DataLoader(train_dataset, shuffle = shuffle, batch_size = batch_size, num_workers = num_workers, pin_memory=True,
-                                            drop_last=True, collate_fn=centernet_dataset_collate, sampler=train_sampler)
+                                            drop_last=True, collate_fn=centernet_dataset_collate, sampler=train_sampler, 
+                                            worker_init_fn=partial(worker_init_fn, rank=rank, seed=seed))
                 gen_val         = DataLoader(val_dataset  , shuffle = shuffle, batch_size = batch_size, num_workers = num_workers, pin_memory=True, 
-                                            drop_last=True, collate_fn=centernet_dataset_collate, sampler=val_sampler)
+                                            drop_last=True, collate_fn=centernet_dataset_collate, sampler=val_sampler, 
+                                            worker_init_fn=partial(worker_init_fn, rank=rank, seed=seed))
 
                 UnFreeze_flag = True
 
